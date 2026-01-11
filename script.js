6d912b94aed2e399a622e35eb70284db9471defa
Ghadir1998 <ghadir.sleem@gmail.com>
2026-01-11 15:00:48 +0100

    changes to js

--- script.js
+++ script.js
@@ -1,118 +1,55 @@
-// Declare variables for getting the xml file for the XSL transformation (folio_xml) and to load the image in IIIF on the page in question (number).
-let tei = document.getElementById("folio");
-let tei_xml = tei.innerHTML;
-let extension = ".xml";
-let folio_xml = tei_xml.concat(extension);
-let page = document.getElementById("page");
-let pageN = page.innerHTML;
-let number = Number(pageN);
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:tei="http://www.tei-c.org/ns/1.0"
+    exclude-result-prefixes="tei"
+    version="1.0">
 
-// Loading the IIIF manifest
-var mirador = Mirador.viewer({
-  "id": "my-mirador",
-  "manifests": {
-    "https://iiif.bodleian.ox.ac.uk/iiif/manifest/53fd0f29-d482-46e1-aa9d-37829b49987d.json": {
-      provider: "Bodleian Library, University of Oxford"
-    }
-  },
-  "window": {
-    allowClose: false,
-    allowWindowSideBar: true,
-    allowTopMenuButton: false,
-    allowMaximize: false,
-    hideWindowTitle: true,
-    panels: {
-      info: false,
-      attribution: false,
-      canvas: true,
-      annotations: false,
-      search: false,
-      layers: false,
-    }
-  },
-  "workspaceControlPanel": {
-    enabled: false,
-  },
-  "windows": [
-    {
-      loadedManifest: "https://iiif.bodleian.ox.ac.uk/iiif/manifest/53fd0f29-d482-46e1-aa9d-37829b49987d.json",
-      canvasIndex: number,
-      thumbnailNavigationPosition: 'off'
-    }
-  ]
-});
+    <xsl:template match="tei:TEI">
+        <div class="row">
+            <div class="col">
+                <h4>About the manuscript page:</h4>
+                <div class="meta-block">
+                    <xsl:value-of select="normalize-space(string(//tei:sourceDesc))"/>
+                </div>
+                <div class="meta-block">
+                    <xsl:value-of select="normalize-space(string(//tei:licence))"/>
+                </div>
+            </div>
 
+            <div class="col">
+                <h4>Statistics:</h4>
+                <ul>
+                    <li>Total number of modifications (adds + dels):
+                        <strong><xsl:value-of select="count(//tei:del | //tei:add)"/></strong>
+                    </li>
 
-// function to transform the text encoded in TEI with the xsl stylesheet "Frankenstein_text.xsl", this will apply the templates and output the text in the html <div id="text">
-function documentLoader() {
+                    <li>Number of additions:
+                        <strong><xsl:value-of select="count(//tei:add)"/></strong>
+                    </li>
 
-    Promise.all([
-      fetch(folio_xml).then(response => response.text()),
-      fetch("Frankenstein_text.xsl").then(response => response.text())
-    ])
-    .then(function ([xmlString, xslString]) {
-      var parser = new DOMParser();
-      var xml_doc = parser.parseFromString(xmlString, "text/xml");
-      var xsl_doc = parser.parseFromString(xslString, "text/xml");
+                    <li>Number of deletions:
+                        <strong><xsl:value-of select="count(//tei:del)"/></strong>
+                    </li>
 
-      var xsltProcessor = new XSLTProcessor();
-      xsltProcessor.importStylesheet(xsl_doc);
-      var resultDocument = xsltProcessor.transformToFragment(xml_doc, document);
+                    <li>Mary Shelley additions (#MWS):
+                        <strong><xsl:value-of select="count(//tei:add[@hand='#MWS'])"/></strong>
+                    </li>
 
-      var criticalElement = document.getElementById("text");
-      criticalElement.innerHTML = ''; // Clear existing content
-      criticalElement.appendChild(resultDocument);
-    })
-    .catch(function (error) {
-      console.error("Error loading documents:", error);
-    });
-  }
-  
-// function to transform the metadate encoded in teiHeader with the xsl stylesheet "Frankenstein_meta.xsl", this will apply the templates and output the text in the html <div id="stats">
-  function statsLoader() {
+                    <li>Mary Shelley deletions (#MWS):
+                        <strong><xsl:value-of select="count(//tei:del[@hand='#MWS'])"/></strong>
+                    </li>
 
-    Promise.all([
-      fetch(folio_xml).then(response => response.text()),
-      fetch("Frankenstein_meta.xsl").then(response => response.text())
-    ])
-    .then(function ([xmlString, xslString]) {
-      var parser = new DOMParser();
-      var xml_doc = parser.parseFromString(xmlString, "text/xml");
-      var xsl_doc = parser.parseFromString(xslString, "text/xml");
+                    <li>Percy Shelley additions (#PBS):
+                        <strong><xsl:value-of select="count(//tei:add[@hand='#PBS'])"/></strong>
+                    </li>
 
-      var xsltProcessor = new XSLTProcessor();
-      xsltProcessor.importStylesheet(xsl_doc);
-      var resultDocument = xsltProcessor.transformToFragment(xml_doc, document);
+                    <li>Percy Shelley deletions (#PBS):
+                        <strong><xsl:value-of select="count(//tei:del[@hand='#PBS'])"/></strong>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+    </xsl:template>
 
-      var criticalElement = document.getElementById("stats");
-      criticalElement.innerHTML = ''; // Clear existing content
-      criticalElement.appendChild(resultDocument);
-    })
-    .catch(function (error) {
-      console.error("Error loading documents:", error);
-    });
-  }
-
-  // Initial document load
-  documentLoader();
-  statsLoader();
-  // Event listener for sel1 change
-  function selectHand(event) {
-  var visible_mary = document.getElementsByClassName('#MWS');
-  var visible_percy = document.getElementsByClassName('#PBS');
-  // Convert the HTMLCollection to an array for forEach compatibility
-  var MaryArray = Array.from(visible_mary);
-  var PercyArray = Array.from(visible_percy);
-    if (event.target.value == 'both') {
-    //write an forEach() method that shows all the text written and modified by both hand (in black?). The forEach() method of Array instances executes a provided function once for each array element.
-     
-    } else if (event.target.value == 'Mary') {
-     //write an forEach() method that shows all the text written and modified by Mary in a different color (or highlight it) and the text by Percy in black. 
-     
-    } else {
-     //write an forEach() method that shows all the text written and modified by Percy in a different color (or highlight it) and the text by Mary in black.
-    
-    }
-  }
-// write another function that will toggle the display of the deletions by clicking on a button
-// EXTRA: write a function that will display the text as a reading text by clicking on a button or another dropdown list, meaning that all the deletions are removed and that the additions are shown inline (not in superscript)
+</xsl:stylesheet>
